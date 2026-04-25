e022a7bdff4fa95e712031d6fa9162f4bf77c536
NotJoshxops <blahyoutun@gmail.com>
2026-04-25 18:19:03 +0100

    Create proper deb package structure

---
+++ debs/Library/Application Support/Rootful/SunshineGlowEffect.swift
@@ -0,0 +1,11 @@
+import UIKit
+import QuartzCore
+
+class SunshineGlowEffect: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
